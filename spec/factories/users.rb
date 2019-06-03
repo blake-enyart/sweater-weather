293fa60463e8657c644404283dcf11cf8b8755c9
Blake <blake.enyart@gmail.com>
2019-06-03 17:45:02 -0600

    Create user for factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password { "MyString" }
+    sequence(:email) { Faker::Internet.email }
+    sequence(:password) { Faker::Internet.password(8) }
+    api_key { Digest::SHA1.hexdigest(email) }
   end
 end
