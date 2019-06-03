c8033159d0956fefbf18778edaabb2e2cb99b227
Blake <blake.enyart@gmail.com>
2019-06-03 09:40:22 -0600

    Add test for antipode

--- spec/services/antipode_service_spec.rb
+++ spec/services/antipode_service_spec.rb
@@ -12,4 +12,28 @@ describe AntipodeService do
   it 'returns attr correctly' do
     expect(antipode_service.city).to eq('Hong Kong')
   end
+
+  it 'gets coordinates based on city name' do
+    expected = {
+                    latitude: 22.3193039,
+                    longitude: 114.1693611
+                }
+
+    expect(antipode_service.geocoded_location).to eq(expected)
+  end
+
+  it 'gets antipode coordinates based on city' do
+    expected = {
+        "data": {
+            "id": "1",
+            "type": "antipode",
+            "attributes": {
+                "lat": -22.3193039,
+                "long": -65.8306389
+            }
+        }
+    }
+
+    expect(antipode_service.get_antipode).to eq(expected)
+  end
 end
