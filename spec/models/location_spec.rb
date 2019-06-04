b9628ed50408f2188541075a109999c512e16c90
Blake <blake.enyart@gmail.com>
2019-06-03 20:57:29 -0600

    Minor modifications to test

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Location, type: :model do
   describe 'validations' do
     it { should validate_presence_of :city}
+    it { should validate_presence_of :latitude}
+    it { should validate_presence_of :longitude}
   end
 
   describe 'relationships' do
