9d551efdd0367dd9f2b48e6ee11a975f2b333f95
Blake <blake.enyart@gmail.com>
2019-06-05 10:01:48 -0600

    Hotfix for postman

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::SessionsController < ApplicationController
     if user && user.authenticate(request_info[:password])
       flash[:success] = "You are now logged in as #{user.email}."
       render json: {
-        api_key: user.api_key,
+        api_key: user.api_key
       }
     else
       flash[:error] = 'Your credentials are incorrect.'
-      render :new # need to build this view out
+      render json: { error: "Incorrect credentials."}
     end
   end
 end
