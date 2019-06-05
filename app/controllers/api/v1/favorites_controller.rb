ccc76ccbfc7e7286273a0af5846d073cb5af004e
Blake <blake.enyart@gmail.com>
2019-06-05 11:22:45 -0600

    Just finishing this.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     request_info = JSON.parse(request.body.read, symbolize_names: true)
-    location = Location.find(request_info[:location])
+    city_state = request_info[:location].split(',').map do |element|
+      element.strip
+    end
+    location = Location.find_by(city: city_state[0].titleize, state: city_state[1].upcase)
     user = User.find_by(api_key: request_info[:api_key])
     if user && request_info[:api_key]
       FavoriteLocation.create(user: user, location: location)
@@ -24,8 +27,12 @@ class Api::V1::FavoritesController < ApplicationController
   def destroy
     request_info = JSON.parse(request.body.read, symbolize_names: true)
     user = User.find_by(api_key: request_info[:api_key])
+    city_state = request_info[:location].split(',').map do |element|
+      element.strip
+    end
+    location = Location.find_by(city: city_state[0].titleize, state: city_state[1].upcase)
     if user && request_info[:api_key]
-      favorite_location = FavoriteLocation.find_by(user_id: user.id, location_id: params[:location])
+      favorite_location = FavoriteLocation.find_by(user_id: user.id, location_id: location)
       favorite_location.delete
       render status: 200, json: FavoriteSerializer.parse(user.fav_city_names)
     else
